413555583bd75a1366bbcd37b9fa866d50e08125
Joe Lohr <jlohr@autorisknow.com>
2023-01-03 07:02:24 -0600

    Category and Post created

---
+++ database/migrations/2023_01_03_065656_create_posts_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id')->constrained();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
