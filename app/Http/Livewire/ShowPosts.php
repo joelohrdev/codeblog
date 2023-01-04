662575b252af755bbef9a56c36a416defc210ace
Joe Lohr <jlohr@autorisknow.com>
2023-01-04 10:44:37 -0600

    Added pagination

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -4,13 +4,15 @@ namespace App\Http\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowPosts extends Component
 {
+    use WithPagination;
     public function render()
     {
         return view('livewire.show-posts', [
-            'posts' => Post::all()
+            'posts' => Post::paginate(10)
         ]);
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,5 +1,9 @@
-<div>
+<div class="space-y-5">
     @foreach($posts as $post)
-        <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+        <div>
+            <a class="text-xl" href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+            <p class="text-sm text-gray-400">{{ $post->created_at->format('F d, Y') }}</p>
+        </div>
     @endforeach
+    {{ $posts->links() }}
 </div>
